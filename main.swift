1924aa6733c0abbb6a6ee4f0e8981b0a48607bc5
Jaella-Oradei <jaellakusonika@hotmail.co.uk>
2025-04-01 13:49:28 +0100

    Print statements to make sure program is running and letting users know the stage in which program is in

---
+++ main.swift
@@ -0,0 +1,43 @@
+//main class file for restaurant finder for just eat ios assigment
+
+
+//import Foundation for accessing networks
+
+import Foundation
+
+//indicating to user that the program has started running and the restaurants are being searched.
+print("Program has started...")
+print("**************************************************")
+print("Finding Restaurants for postcode EC3N 4DJ...")
+
+
+
+//decoding the response (API)
+/*
+ - Struct API_Response
+ 
+ 
+ - Struct Restaurant
+ 
+ 
+ - Struct Cuisine
+ 
+ 
+ 
+ - Struct Ratings
+ 
+ 
+ - Struct Address
+ 
+ 
+ 
+ */
+
+
+
+//finding restaurants from api
+
+
+
+
+//printing top 10 restaurants with attributes (cusinine, rating & address)
