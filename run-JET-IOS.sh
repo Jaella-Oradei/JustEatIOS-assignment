c27deb035e717e32ba19a686a04cc9ff4d03d8d0
Jaella-Oradei <jaellakusonika@hotmail.co.uk>
2025-04-06 13:54:18 +0100

    removed .sh file as it is not working to make code run from calling it in the script

--- run-JET-IOS.sh
+++
@@ -1,9 +0,0 @@
-
-
-#resizing the terminal window to open in desired size for the format of the output to be correct.
-
-printf 'e\[8;125;54t'
-
-echo "Resizing terminal for optimal display & running program"
-
-Swift main.swift
